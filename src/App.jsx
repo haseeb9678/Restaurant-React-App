4c90632885db159905667894af47d8319a0a64b5
Haseeb Tariq <haseebtariq963100@gmail.com>
2025-08-28 19:49:04 +0500

    improve nav width

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <div className='w-[80%] mx-auto flex flex-col gap-5'>
+    <div className='w-[95%] mx-auto flex flex-col gap-5'>
       <Navbar />
       <Hero />
     </div>
